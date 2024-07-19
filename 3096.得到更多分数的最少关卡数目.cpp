f98cf2844ca27d3ec2f5f363c2c423e023f57e48
ber <bai5775@outlook.com>
2024-07-19 15:56:52 +0800

    ==> 2024-07-19 15:56:52 commit <==

---
+++ 3096.得到更多分数的最少关卡数目.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=3096 lang=cpp
+ *
+ * [3096] 得到更多分数的最少关卡数目
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minimumLevels(vector<int>& possible) {
+        
+    }
+};
+// @lc code=end
+
