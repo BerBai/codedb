c981f06d88544f5484907fc0794e7aaa0545fc3f
ber <bai5775@outlook.com>
2024-11-08 14:10:57 +0800

    ==> 2024-11-08 14:10:57 commit <==

--- 179.最大数.py
+++ 179.最大数.py
@@ -10,8 +10,9 @@ class Solution:
         ans = []
         for num in nums:
             ans.append(str(num))
-        # 先将字符重复10次，在按字典序降序排序
+        # 先将字符重复10次，在按字典序降序排序 避免3 30，出现 303 这样的情况
         ans.sort(key=lambda x: x*10, reverse=True)
+        # 转成int 去除000的情况 转成str得到答案
         return str(int(''.join(ans)))
 # @lc code=end
 
