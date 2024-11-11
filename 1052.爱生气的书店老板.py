fb12ddf04423f3b8d552431894de79a601d0714b
ber <bai5775@outlook.com>
2024-11-11 16:08:01 +0800

    ==> 2024-11-11 16:08:01 commit <==

--- 1052.爱生气的书店老板.py
+++ 1052.爱生气的书店老板.py
@@ -7,6 +7,22 @@
 # @lc code=start
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
-        
+        n = len(customers)
+        ans = 0
+        for i in range(n):
+            if grumpy[i] == 0:
+                ans += customers[i]
+                customers[i] = 0
+        l = r = 0
+        cur = 0
+        max_ = 0
+        while r < n:
+            cur += customers[r]
+            r += 1
+            if r - l == minutes:
+                max_ = max(max_, cur)
+                cur -= customers[l]
+                l += 1
+        return ans + max_
 # @lc code=end
 
