ca4f65e2270d3aa7298af759fe4261f6baa0ec18
ber <bai5775@outlook.com>
2024-11-20 17:43:50 +0800

    ==> 2024-11-20 17:43:50 commit <==

--- HW/【固定滑窗】2024E-关联字串 优化.py
+++ HW/【固定滑窗】2024E-关联字串 优化.py
@@ -18,12 +18,22 @@ def cal_cnt(s):
 
 l, r = 0, len(target)
 tarCnt = cal_cnt(target)
+cnt = cal_cnt(s[l:r])
 while r <= len(s):
-    cnt = cal_cnt(s[l:r])
     if cnt == tarCnt:
         print(l)
         exit()
+    cnt[s[l]] -= 1
+    if cnt[s[l]] == 0:
+        del cnt[s[l]]
+
+    if r < len(s) and s[r] not in cnt:
+        cnt[s[r]] = 1
+    elif r < len(s):
+        cnt[s[r]] += 1
+    
     l += 1
     r += 1
+
 print(-1)
 # @hw code=end
