2ef71e01124f289c1a6e3a92b1806d59a700201f
ber <bai5775@outlook.com>
2024-11-25 15:24:02 +0800

    ==> 2024-11-25 15:24:01 commit <==

--- HW/【BFS】2024E-流浪地球.py
+++ HW/【BFS】2024E-流浪地球.py
@@ -5,26 +5,33 @@
 #
 
 # @hw code=start
-n, m = map(int, input().split())
-arr = {}
-for i in range(m):
-    k, v = map(int, input().split())
-    if k not in arr:
-        arr[k] = [v]
-    else:
-        arr[k].append(v)
+from collections import deque
 
-queue = arr[0]
+n, m = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(m)]
+
+dt = {}
+for t, i in arr:
+    if t not in dt:
+        dt[t] = [i]
+    else:
+        dt[t].append(i)
 
 vis = []
 time = 0
-while len(queue) > 0:
-
-    time += 1
+queue = deque()
+while time < n:
+    # 点火 入队
+    if time in dt:
+        for i in dt[time]:
+            if i not in vis and i not in queue:
+                queue.append(i)
+    
+    # 同一时间触发的出队
     k = len(queue)
     while k > 0:
         k -= 1
-        cur = queue.pop(0)
+        cur = queue.popleft()
         vis.append(cur)
         l = (cur - 1 + n) % n
         r = (cur + 1) % n
@@ -32,12 +39,12 @@ while len(queue) > 0:
             queue.append(l)
         if r not in vis and r not in queue:
             queue.append(r)
-    if time in arr:
-        for num in arr[time]:
-            if num not in vis and num not in queue:
-                queue.append(num)
+    
     if len(queue) + len(vis) == n:
             break
+    time += 1
 print(len(queue))
+# 输入需要升序排序
+queue = sorted(queue)
 print(' '.join([str(num) for num in queue]))
 # @hw code=end
