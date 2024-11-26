be78473469dea96f45da6afa0a624df227d2dd24
ber <bai5775@outlook.com>
2024-11-26 10:18:50 +0800

    ==> 2024-11-26 10:18:49 commit <==

--- HW/【BFS】【拓扑排序】2024E-BOSS的收入.py
+++ HW/【BFS】【拓扑排序】2024E-BOSS的收入.py
@@ -5,5 +5,31 @@
 # 
 
 # @hw code=start
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+# 构图
+dit = {}
+# 记录节点收入
+values = {}
+# 记录节点入度
+cnts = {}
+
+for j, i, c in arr:
+    if i not in dit:
+        dit[i] = [j]
+    if j not in dit:
+        dit[j] = []
+    values[j] = c
+
+vis = [0] * n
+
+def dfs(cur):
+    if vis[cur] == 1:
+        return
+    vis[cur] = 1
+    for v in dit[cur]:
+        d
+
 
 # @hw code=end
