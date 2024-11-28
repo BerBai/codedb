d0498dfb45a7c7806bf26c010e36b3a74237f73c
ber <bai5775@outlook.com>
2024-11-28 17:27:51 +0800

    ==> 2024-11-28 17:27:51 commit <==

--- HW/【DFS-BFS】2024E-可以组成网络的服务器.py
+++ HW/【DFS-BFS】2024E-可以组成网络的服务器.py
@@ -7,7 +7,7 @@
 # @hw code=start
 from collections import deque
 
-def find(arr):
+def find(n, m, arr):
     queue = deque()
     vis = [[False] * m for _ in range(n)]
     max_m = 0
@@ -16,24 +16,26 @@ def find(arr):
             if arr[i][j]:
                 queue.append([i, j])
                 vis[i][j] = True
-                m = bfs(queue, n, m, arr, vis)
-                max_m = max(max_m, m)
+                mm = bfs(queue, n, m, arr, vis)
+                max_m = max(max_m, mm)
     return max_m
 
 def bfs(queue, n, m, arr, vis):
     directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    m = 1
+    ma = 1
     while queue:
         i, j = queue.popleft()
         for x, y in directions:
             ni, nj = i + x, j + y
             if 0 <= ni < n and 0 <= nj < m and not vis[ni][nj] and arr[ni][nj]:
                 vis[ni][nj] = True
-                m += 1
+                ma += 1
                 queue.append([ni, nj])
-    return m
+    return ma
 
 n, m = map(int, input().split())
-arr = [list(map(int, input().split()))]
+arr = [list(map(int, input().split())) for _ in range(n)]
 
+max_m = find(n, m, arr)
+print(max_m)
 # @hw code=end
