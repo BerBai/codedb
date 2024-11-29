eff117503ca3fc8ed9b7f64e0b3b50881b0ec1c2
ber <bai5775@outlook.com>
2024-11-29 12:50:30 +0800

    ==> 2024-11-29 12:50:29 commit <==

--- HW/【DP】2024E-分披萨.py
+++ HW/【DP】2024E-分披萨.py
@@ -8,29 +8,33 @@
 
 # 区间 [l, r] 里，“吃货”能分得的最大披萨大小的总和, t剩余数
 def cal_max(l, r, t):
-    if t <= 1:
-        mem[l][r][t] = 0
-        return 0
-    # if mem[l][r][t] != -1:
-    #     return mem[l][r][t]
+    global arr
 
-    # 馋嘴选最大的
-    if arr[l] > arr[r]:
+    if t <= 1:
+        return 0
+    
+    if mem[l][r][t] != -1:
+        return mem[l][r][t]
+
+    # 馋货选最大的
+    if arr[l] < arr[r]:
         r = (r + 1) % n
     else:
         l = (l - 1 + n) % n
-
-    # 吃货两种选择
+    
+    # 吃货的两种选择
     left = arr[l] + cal_max((l - 1 + n) % n, r, t - 2)
     right = arr[r] + cal_max(l, (r + 1) % n, t - 2)
+
     mem[l][r][t] = max(left, right)
     return mem[l][r][t]
 
 n = int(input())
 arr = [int(input()) for _ in range(n)]
-mem = [[[-1] * n] * n] * n
 ans = 0
+
 for i in range(n):
+    mem = [[[-1] * n] * n] * n
     ans = max(ans, arr[i] + cal_max((i - 1 + n) % n, (i + 1) % n, n - 1))
 
 print(ans)
